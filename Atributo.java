caef20cb94ce5d961b6284958cb0ce42c4880f53
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-10-21 17:46:35 +0000

    Arquivos

---
+++ Atributo.java
@@ -0,0 +1,7 @@
+public class Atributo{
+
+    private int numero;
+    public int numero2;
+    int numero3;
+    protected int numero4;
+}
---
+++ Atributo2.java
@@ -0,0 +1,5 @@
+public class Atributo2 extends Atributo{
+
+
+
+}
